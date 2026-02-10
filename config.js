88f69042e764e8ace4ebd395e83e1cfbec988f56
VictorOlapido <enzo.magdato@student.ateneo.edu>
2026-02-10 12:27:04 +0800

    Update config.js

--- config.js
+++ config.js
@@ -81,7 +81,7 @@ const CONFIG = {
 // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
-        volume: 0.5                        // Volume level (0.0 to 1.0)
+        volume: 0.0                       // Volume level (0.0 to 1.0)
     }
 };
 
